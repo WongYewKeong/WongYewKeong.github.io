ab525a38dd8a4f3176a5731c4b086af3a7b38ee7
WongYewKeong <a175308@siswa.ukm.edu.my>
2022-05-18 15:51:55 +0800

    Complete portfolio

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -5,17 +5,14 @@ import { FaFacebookF } from 'react-icons/fa';
 const SocialMedia = () => (
   <div className="app__social">
     <div>
-      <a href='https://www.linkedin.com/in/wong-yew-keong/'>
+      <a href='https://www.linkedin.com/in/wong-yew-keong/' target="_blank" rel="noreferrer">
       <BsLinkedin />
       </a>
     </div>
     <div>
-      <a href='https://github.com/WongYewKeong'>
+      <a href='https://github.com/WongYewKeong' target="_blank" rel="noreferrer">
       <BsGithub />
       </a>
-    </div>
-    <div>
-      <FaFacebookF />
     </div>
   </div>
 );
