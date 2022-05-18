f1bcfc54e472453d275c43b469b5ce8974a90f93
WongYewKeong <a175308@siswa.ukm.edu.my>
2022-05-18 17:13:44 +0800

    Edit scss

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BsLinkedin, BsGithub } from 'react-icons/bs';
-import { FaFacebookF } from 'react-icons/fa';
 
 const SocialMedia = () => (
   <div className="app__social">
