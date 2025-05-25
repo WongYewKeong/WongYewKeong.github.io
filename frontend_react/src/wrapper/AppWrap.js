c9b43b24d483b6ebc30f33b19bb47b62da48ee50
WongYewKeong <a175308@siswa.ukm.edu.my>
2025-05-25 13:31:31 +0800

    copyright

--- frontend_react/src/wrapper/AppWrap.js
+++ frontend_react/src/wrapper/AppWrap.js
@@ -9,7 +9,7 @@ const AppWrap = (Component, idName, classNames) => function HOC() {
           <Component />
   
           <div className="copyright">
-            <p className="p-text">©2024 Wong Yew Keong</p>
+            <p className="p-text">©2025 Wong Yew Keong</p>
             <p className="p-text">All rights reserved</p>
           </div>
         </div>
